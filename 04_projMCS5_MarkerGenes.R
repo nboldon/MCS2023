b3b0ee438fdf4dc2f2fdefc9b7808112eb8905a8
Naomi Boldon <98961232+nboldon@users.noreply.github.com>
2025-03-06 16:31:38 -0700

    Create 04_projMCS5_MarkerGenes.R
    Includes abs(Log2FC) for getMarkerFeatures by Cluster using all MCS2023 sample cells.

---
+++ 04_projMCS5_MarkerGenes.R
@@ -0,0 +1,5 @@
+
+## NOTE: code for 04_projMCS2_MarkerGenes.R was run using Log2FC>=1.25 (not abs)
+## This code was run later using projMCS5 and includes abs(Log2FC)
+# Initial marker gene summaries were created using FDR values (not Log2FC)
+
